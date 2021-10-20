693ce284d7ca93c64add14d8aa0246a02c183171
jpate101 <62268987+jpate101@users.noreply.github.com>
2021-10-20 18:24:36 +1000

    SA_v1

--- routes/api.js
+++ routes/api.js
@@ -52,6 +52,32 @@ router.get("/:query", (req, res) => {
       //if an error happens then render an error page
       res.end();
     })
+
+    let testing_tweet = "textBlob sure looks like it has some interresting features";
+    
+    let { spawn } = require('child_process')
+
+    let process2 = spawn('python',['./SA.py', testing_tweet.toString()]);
+
+    console.log('here start');
+
+    process2.stdout.on('data', (data) => {
+        //console.log('stdout: ${data}');
+        console.log(data.toString());
+        temp = data.toString().split("\n");
+        console.log(temp[0]);
+        console.log(temp[1]);
+        console.log("___________");
+    });
+
+    process2.stderr.on('data', (data) => {
+        //console.log('stderr: ${data}');
+        console.log(data.toString());
+    });
+
+    console.log('here end');
+
+
   });
 
   module.exports = router;
