ebb1d22ec5e4b72059c69ed9820292f2c0975efd
benthebest5 <56103296+benthebest5@users.noreply.github.com>
2021-10-26 17:30:09 +1000

    added timeout

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,23 @@ const needle = require('needle');
 const key = require("./keys");
 const express = require('express');
 const router = express.Router();
+const AWS = require('aws-sdk');
 
 const endpointUrl = `https://api.twitter.com/2/tweets/search/recent`;
+
+require('dotenv').config(); 
+
+const bucketName = 'semple-paterson-store';
+
+// Create a promise on S3 service object 
+const bucketPromise = new AWS.S3({apiVersion: '2006-03-01'}).createBucket({Bucket: bucketName}).promise(); 
+bucketPromise.then(function(data) { 
+    console.log("Successfully created " + bucketName); 
+})
+.catch(function(err) {
+    console.error(err, err.stack);
+});
+
 
 async function getRequestTwitter(search) {
 
@@ -102,59 +117,118 @@ async function perform_SA(message) {
 
     return result;
   }
+
   router.get("/:query", (req, res) => {
 
     //set the parameter form url as a variable for convienance
     search = req.params.query;
-  
-    //call the apis
-    apicall().then((value) => {
 
-      console.log(value)
+    const d = new Date();
 
-      console.log("___________recall__test ____________")
+      const s3Key = `twitter-${search}`; 
 
-      let current_polarity_total = 0;
+      const paramss3 = {Bucket: bucketName, Key: s3Key};
 
-      for (let i = 0; i < value[1].length; i++) {
-        console.log(parseFloat(value[1][i][0][1]));
+      new AWS.S3({apiVersion: '2006-03-01'}).getObject(paramss3, (err, result) => {
+        console.log(result);
+        console.log(err);
 
-        // if(parseFloat((value[1][i][0][1]) > 0.1 && !(parseFloat(value[1][i][0][1]) < 0 )) || (parseFloat(value[1][i][0][1]) < -0.1 && !(parseFloat(value[1][i][0][1]) > 0))){
-        if(parseFloat(value[1][i][0][1]) > 0.1 ){
+        let resultJSON;
 
-        current_polarity_total += parseFloat(value[1][i][0][0]);
+        let blank = false;
 
-          console.log("value")
-          console.log(parseFloat(value[1][i][0][0]));
+        if (result){
+          resultJSON = JSON.parse(result.Body);
+
+          if (resultJSON.time + 60000 > d.getTime()) {
+
+            res.render("api", {
+              tweets: resultJSON.tweets,
+              sentiment: resultJSON.polarity,//doesnt work 
+              query: resultJSON.search
+            });
+            res.end();
+          }
+          else {
+            blank = true;
+          }
         }
-      }
+        else{
+          blank = true;
+        }
 
-      console.log(current_polarity_total);
+        if (blank === true) {
 
-      let polarity = "";
+          //call the apis
+          apicall().then((value) => {
 
-      if(current_polarity_total > 0.25){
-        polarity = "Positive";
+          let body = {};
 
-      }else if (current_polarity_total < -0.25 ) {
-        polarity = "Negitive";
-      } else {
-        polarity = "Neutral";
-      }
+          console.log("___________recall__test ____________")
 
-      res.render("api", {
-        tweets: value[0],
-        sentiment: polarity,//doesnt work 
-        query: search
-      });
-      res.end();
+          let current_polarity_total = 0;
 
-    }).catch((e) => {
-      //if an error happens then render an error page
-      res.render("error");
-      res.end();
-    })
+          for (let i = 0; i < value[1].length; i++) {
+            console.log(parseFloat(value[1][i][0][1]));
+
+            // if(parseFloat((value[1][i][0][1]) > 0.1 && !(parseFloat(value[1][i][0][1]) < 0 )) || (parseFloat(value[1][i][0][1]) < -0.1 && !(parseFloat(value[1][i][0][1]) > 0))){
+            if(parseFloat(value[1][i][0][1]) > 0.1 ){
+
+            current_polarity_total += parseFloat(value[1][i][0][0]);
+
+              console.log("value")
+              console.log(parseFloat(value[1][i][0][0]));
+            }
+          }
+
+          console.log(current_polarity_total);
+
+          let polarity = "";
+
+          if(current_polarity_total > 0.25){
+            polarity = "Positive";
+
+          }else if (current_polarity_total < -0.25 ) {
+            polarity = "Negitive";
+          } else {
+            polarity = "Neutral";
+          }
+
+          body["tweets"] = value[0];
+          body["polarity"] = polarity;
+          body["search"] = search;
+          body["time"] = d.getTime();
+
+          body = JSON.stringify(body);
+
+          console.log(body);
+
+          const upparams = {Bucket: bucketName, Key: s3Key, Body: body};
+
+          const upload = new AWS.S3({apiVersion: '2006-03-01'}).putObject(upparams).promise();
+
+          upload.then(function(data) {
+            console.log(data);
+          });
+
+          res.render("api", {
+            tweets: value[0],
+            sentiment: polarity,//doesnt work 
+            query: search
+          });
+          res.end();
+
+        // ).catch((e) => {
+        //   //if an error happens then render an error page
+        //   res.render("error");
+        //   res.end();
+        // })
+    
+
+    });
+  }
 
   });
+});
 
 module.exports = router;
